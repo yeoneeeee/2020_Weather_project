accb490d7ccbad810da75b754b183e55f751bc86
Below0 <l4538@naver.com>
2020-06-30 11:16:40 +0900

    complete weather service

--- src/server/batch/midRefresh.py
+++ src/server/batch/midRefresh.py
@@ -4,59 +4,13 @@ import urllib.request
 import time
 from datetime import datetime, timedelta
 
+conn = pymongo.MongoClient('127.0.0.1', 27017)
+db = conn.get_database('scsc')
+weather_col = db.get_collection('test')
+score_col = db.get_collection('score')
 
 with open("./config/config.json", "r") as sk_json:
     service_key = json.load(sk_json)['key']
-
-
-class ShortWeatherService:
-
-    def make_record(self, regID='11B10101'):
-        result_dict = {}
-        address = "http://apis.data.go.kr/1360000/VilageFcstMsgService/getLandFcst?serviceKey=" + service_key + "&numOfRows=10&pageNo=1&numOfRows=10&pageNo=1&dataType=JSON"
-        req = urllib.request.urlopen(address + "&regId=" + regID)
-        res = req.readline()
-
-        j = json.loads(res)
-
-        if j['response']['header']['resultCode'] != '00':
-            print('API CALL Failure')
-            return
-
-        j = j['response']['body']['items']['item']
-
-        date = datetime.now()
-        announce_time = str(j[0]['announceTime'])[-4:]
-        result = []
-
-        if announce_time == "1100":
-            for numEf in range(0, 5):
-                print(j[numEf])
-
-
-        elif announce_time == "0500":
-            for numEf in range(0, 6):
-                print(j[numEf])
-
-
-        elif announce_time == "1700":
-            for numEf in range(1, 5):
-                if numEf % 2 == 1:
-                    record = {}
-                    target_date = (date + timedelta(days=numEf / 2)).strftime('%Y%m%d')
-                    record['date'] = target_date
-                    record['regID'] = regID
-                    record['rnStAm'] = j[numEf]['rnSt']
-                    record['wfAm'] = j[numEf]['wf']
-                    record['taMin'] = j[numEf]['ta']
-
-                else:
-                    record['rnStPm'] = j[numEf]['rnSt']
-                    record['wfPm'] = j[numEf]['wf']
-                    record['taMax'] = j[numEf]['ta']
-                    result.append(record)
-
-        return result
 
 
 class MidWeatherService:  # 중기예보 서비스 모듈
@@ -94,7 +48,6 @@ class MidWeatherService:  # 중기예보 서비스 모듈
     def get_temperature(self, regID='11B10101'):
         address = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey=" + service_key + "&numOfRows=10&pageNo=1&dataType=JSON"
         req = urllib.request.urlopen(address + self.make_tmfc() + "&regId=" + regID)
-        print(address + self.make_tmfc() + "&regId=" + regID)
         res = req.readline()
 
         j = json.loads(res)
@@ -148,18 +101,8 @@ class MidWeatherService:  # 중기예보 서비스 모듈
         return result
 
 
-conn = pymongo.MongoClient('127.0.0.1', 27017)
-db = conn.get_database('scsc')
-collection = db.get_collection('test')
-
-s_service = ShortWeatherService()
 m_service = MidWeatherService()
+res = m_service.make_record()
 
-short_res = s_service.make_record()
-mid_res = m_service.make_record()
-
-upserts = [ pymongo.UpdateOne({'date': x['date'], 'regID':x['regID']}, {'$set': x}, upsert=True) for x in short_res]
-result = collection.bulk_write(upserts)
-
-upserts = [ pymongo.UpdateOne({'date': x['date'], 'regID':x['regID']}, {'$set': x}, upsert=True) for x in mid_res]
-result = collection.bulk_write(upserts)
+bulk_list = [pymongo.UpdateOne({'date': x['date'], 'regID': x['regID']}, {'$set': x}, upsert=True) for x in res]
+result = weather_col.bulk_write(bulk_list)
