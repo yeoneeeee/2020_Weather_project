5f2439c9a6b0affa6ab2e37065e5db60289a6ae1
Below0 <l4538@naver.com>
2020-06-30 16:29:25 +0900

    add cleaner score

--- src/server/batch/shortRefresh.py
+++ src/server/batch/shortRefresh.py
@@ -6,11 +6,69 @@ from datetime import datetime, timedelta
 
 conn = pymongo.MongoClient('127.0.0.1', 27017)
 db = conn.get_database('scsc')
-weather_col = db.get_collection('test')
+weather_col = db.get_collection('weather')
 score_col = db.get_collection('score')
 
 with open("./config/config.json", "r") as sk_json:
     service_key = json.load(sk_json)['key']
+
+
+class ScoreCaculator:
+
+    def __init__(self, rec):
+        self.rec = list(rec)
+        self.lv_list = []
+        self.cnt = rec.count()
+
+    def calc_ta(self, i):
+        line = self.rec[i]
+        ta_min = int(line.get('taMin', 10))
+        ta_max = int(line.get('taMax', 25))
+
+        if ta_min < -10 or ta_max > 35:
+            ta_level = -5
+        elif ta_min < 0 or ta_max > 33:
+            ta_level = 0
+        elif ta_min < 5 or ta_max > 28:
+            ta_level = 3
+        elif ta_min < 10:
+            ta_level = 5
+        else:
+            ta_level = 7
+        return ta_level
+
+    def calc_rn(self, value):
+        if value is None:
+            return 3
+
+        temp = int(value)
+        return 5-int(temp/20)
+
+    def make_rnlv(self):
+
+        for i in range(len(self.rec)):
+            am_lv = self.calc_rn(self.rec[i].get('rnStAm', None))
+            pm_lv = self.calc_rn(self.rec[i].get('rnStPm', None))
+            self.lv_list.append((am_lv, pm_lv))
+
+    def run(self):
+        result = []
+        self.make_rnlv()
+
+        for i in range(0, self.cnt-3):
+            line = self.rec[i]
+            score = 0
+            doc = {
+                'date': line['date'],
+                'regID': line['regID'],
+                   }
+            for j in range(i, i+4):
+                pivot = min(self.lv_list[j])
+                score += pivot*(i+5-j)
+            score += self.calc_ta(i)
+            doc['score'] = score
+            result.append(doc)
+        return result
 
 
 class ShortWeatherService:
@@ -18,7 +76,6 @@ class ShortWeatherService:
     def make_record(self, regID='11B10101'):
         result = []
         date = datetime.now()
-        time_delta = 0
         address = "http://apis.data.go.kr/1360000/VilageFcstMsgService/getLandFcst?serviceKey=" + service_key + "&numOfRows=10&pageNo=1&numOfRows=10&pageNo=1&dataType=JSON"
 
         req = urllib.request.urlopen(address + "&regId=" + regID)
@@ -77,3 +134,10 @@ res = s_service.make_record()
 bulk_list = [pymongo.UpdateOne({'date': x['date'], 'regID': x['regID']}, {'$set': x}, upsert=True) for x in res]
 weather_col.bulk_write(bulk_list)
 print("Update short weather")
+
+rec = weather_col.find({"date": {"$gte": datetime.now().strftime('%Y%m%d')}})
+calculator = ScoreCaculator(rec)
+res = calculator.run()
+bulk_list = [pymongo.UpdateOne({'date': x['date'], 'regID': x['regID']}, {'$set': x}, upsert=True) for x in res]
+score_col.bulk_write(bulk_list)
+print("Update cleaner score")
