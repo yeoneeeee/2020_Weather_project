a3bc560f3ccc6e16d58cf1211a7faf96e2b153a3
nayajiwon <littlecherub@naver.com>
2020-07-05 15:20:05 +0900

    add client

---
+++ src/client/newcar/app/src/main/java/kr/co/company/newcar/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.co.company.newcar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
